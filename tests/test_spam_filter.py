186b8359b0efab9fe69bc123f98e8a18998882bd
spamshieldx <spamshieldxgaint@gmail.com>
2025-08-14 12:48:21 +0530

    Update test_spam_filter.py

--- tests/test_spam_filter.py
+++ tests/test_spam_filter.py
@@ -1,7 +1,7 @@
-from app.spam_filter import is_spam
+from app.spam_filter import classify_spam
 
 def test_inbox_email():
-    assert is_spam("This is a notice from the university") == "Inbox"
+    assert classify_spam("This is a notice from the university") == "Inbox"
 
 def test_spam_email():
-    assert is_spam("Congratulations! You've won a cash prize") == "Spam"
+    assert classify_spam("Congratulations! You've won a cash prize") == "Spam"
