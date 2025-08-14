c4a5a560fe9ea54c635d86174642888f5b35906f
spamshieldx <spamshieldxgaint@gmail.com>
2025-08-14 12:47:09 +0530

    Update spam_filter.py

--- app/spam_filter.py
+++ app/spam_filter.py
@@ -1,6 +1,7 @@
 def classify_email(text):
     university_keywords = ['university', 'professor','congratulations', 'course', 'exam', 'semester', 'department']
-    spam_keywords = ['cash prize', 'win now', 'click here', 'congratulations', 'lottery', 'urgent', 'phone', 'links', 'card details', 'otp', 'link', 'credentials', 'details', 'you won']
+    spam_keywords = ['cash prize', 'win now', 'click here', 'congratulations', 'lottery', 'urgent',
+                     'phone', 'links', 'card details', 'otp', 'link', 'credentials', 'details', 'you won']
     junk_keywords = ['zee5', 'hotstar', 'netflex']
 
     text_lower = text.lower()
@@ -8,11 +9,12 @@ def classify_email(text):
         return "Inbox"
     elif any(word in text_lower for word in spam_keywords):
         return "Spam"
-    elif any(word in text_lower for word in junk_keywords) :
+    elif any(word in text_lower for word in junk_keywords):
         return "Junk"
     return "Inbox"
-def is_spam(snippet, spam_keywords=None):
-    if not spam_keywords:
-        return False
-    snippet = snippet or ""
+
+def classify_spam(snippet):
+    spam_keywords = ['cash prize', 'win now', 'click here', 'congratulations', 'lottery', 'urgent',
+                     'phone', 'links', 'card details', 'otp', 'link', 'credentials', 'details', 'you won']
+    snippet = (snippet or "").lower()
     return any(keyword in snippet for keyword in spam_keywords)
