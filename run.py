aa38867dce8a8755697697638bdc55214f477c91
spamshieldx <spamshieldxgaint@gmail.com>
2025-08-14 12:48:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from app.routes import main as main_bp
 import os
+from app import create_app
 
+app = create_app()
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'app', 'templates'))
 app.secret_key = 'your-secret-key'
 app.register_blueprint(main_bp)
